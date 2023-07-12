ad40532b9f2f634c605e337dff73ac6e3abd7751
robert owino <97714329+robert-owino@users.noreply.github.com>
2023-07-12 14:05:46 +0300

    Add files via upload

---
+++ src/presentDrinks.js
@@ -0,0 +1,19 @@
+
+import fetchDrinks from "./fetchDrinks.js";
+import displayDrinks from './displayDrinks.js'
+import setDrink from "./setDrink.js";
+
+
+const showDrinks = async (url) => {
+  //fetch drinks
+  const data = await fetchDrinks(url);
+ 
+
+  //display drinks
+  const section = displayDrinks(data)
+ if(section){
+  setDrink(section)
+ }
+}
+
+export default showDrinks
